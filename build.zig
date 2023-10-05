77de7030a3ffdc4bdf74abe499ff199416cf6b4d
Dwi Andhika <53227252+siveroo@users.noreply.github.com>
2023-10-05 22:00:45 +0700

    Update build.zig

--- build.zig
+++ build.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 
 pub fn build(b: *std.Build) void {
     var lib = b.addStaticLibrary(.{
-        .name = "tree-sitter",
+        .name = "treesitter",
         .target = b.standardTargetOptions(.{}),
         .optimize = b.standardOptimizeOption(.{}),
     });
